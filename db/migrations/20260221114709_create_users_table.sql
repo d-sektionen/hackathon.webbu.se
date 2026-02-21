f4d19e086d9c2eee7cc77049212aa2ef78b0f392
Otto Roming <otro@lysator.liu.se>
2026-02-21 13:48:27 +0100

    migration

--- db/migrations/20260221114709_create_users_table.sql
+++ db/migrations/20260221114709_create_users_table.sql
@@ -1,5 +1,11 @@
 -- migrate:up
-
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+);
 
 -- migrate:down
-
+DROP TABLE users;
